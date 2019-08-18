bc5961d5824d16a55e418fdf9f23d15f0e2dd100
Yoshdia <yoshiatsu02@gmail.com>
2019-08-18 18:53:04 +0900

    プロジェクトの挿入。プレイヤーの移動やカメラの処理などを追加済み

---
+++ 4thGame-Action/Assets/RPG Character Animation Pack FREE/Code/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{   
+    //追従するオブジェクト
+    public GameObject player;
+    //追従させるカメラオブジェクト
+    public GameObject mainCamera;
+    //カメラを動かすキー(右
+    const int RotateBotton = 1;
+    //回転角度の最小最大値
+    const float AngleLimitMax = 60f;
+    const float AngleLimitMin = -60f;
+    //回転速度
+    [SerializeField]
+    private float rotate_speed = 1.0f;
+
+    void Awake()
+    {
+        mainCamera = Camera.main.gameObject;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        //ターゲットの座標を追従
+        transform.position = player.transform.position;
+
+        //移動用キーが押されていたら
+        if (Input.GetMouseButton(RotateBotton))
+        {
+            rotateCameraAngle();
+        }
+
+        float angle_x = 180f <= transform.eulerAngles.x ? transform.eulerAngles.x - 360 : transform.eulerAngles.x;
+        transform.eulerAngles = new Vector3(
+            Mathf.Clamp(angle_x, AngleLimitMin, AngleLimitMax),
+            transform.eulerAngles.y,
+            transform.eulerAngles.z
+        );
+    }
+    private void rotateCameraAngle()
+    {
+        Vector3 angle = new Vector3(
+            Input.GetAxis("Mouse X") * rotate_speed,
+            Input.GetAxis("Mouse Y") * -rotate_speed,
+            0
+        );
+
+        transform.eulerAngles += new Vector3(angle.y, angle.x);
+    }
+    
+}
---
+++ 4thGame-Action/Assets/RPG Character Animation Pack FREE/Code/RPGCharacterInputControllerFREE.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RPGCharacterAnims{
+	
+	public class RPGCharacterInputControllerFREE : MonoBehaviour{
+
+		//入力情報が格納される変数
+		[HideInInspector] public bool inputJump;
+		[HideInInspector] public bool inputLightHit;
+		[HideInInspector] public bool inputDeath;
+		[HideInInspector] public bool inputAttackL;
+		[HideInInspector] public bool inputAttackR;
+		[HideInInspector] public bool inputSwitchUpDown;
+		[HideInInspector] public bool inputStrafe;
+		[HideInInspector] public float inputAimVertical = 0;
+		[HideInInspector] public float inputAimHorizontal = 0;
+		[HideInInspector] public float inputHorizontal = 0;
+		[HideInInspector] public float inputVertical = 0;
+		[HideInInspector] public bool inputRoll;
+
+		//変数
+
+		[HideInInspector] public bool allowedInput = true;
+        //カメラから見てどの方向に移動するかのベクトル
+		[HideInInspector] public Vector3 moveInput;
+        //入力状態0,1のみを格納
+		[HideInInspector] public Vector2 aimInput;
+
+        ///<param name="Int1">引数１</param>
+        ///<returns></returns>
+        
+        /// <summary>
+        ///Input情報の取得
+        /// </summary>    
+        void Inputs(){
+			inputJump = Input.GetButtonDown("Jump");
+			//inputLightHit = Input.GetButtonDown("LightHit");
+			//inputDeath = Input.GetButtonDown("Death");
+			//inputAttackL = Input.GetButtonDown("AttackL");
+			//inputAttackR = Input.GetButtonDown("AttackR");
+			//inputSwitchUpDown = Input.GetButtonDown("SwitchUpDown");
+			inputStrafe = Input.GetKey(KeyCode.LeftShift);
+			//inputAimVertical = Input.GetAxisRaw("AimVertical");
+			//inputAimHorizontal = Input.GetAxisRaw("AimHorizontal");
+			inputHorizontal = Input.GetAxisRaw("Horizontal");
+			inputVertical = Input.GetAxisRaw("Vertical");
+			//inputRoll = Input.GetButtonDown("L3");
+		}
+
+		void Awake(){
+			allowedInput = true;
+		}
+
+		void Update(){
+			Inputs();
+			moveInput = CameraRelativeInput(inputHorizontal, inputVertical);
+			aimInput = new Vector2(inputAimHorizontal, inputAimVertical);
+		}
+
+        /// <summary>
+        /// カメラの向きに基づいて動かす
+        /// </summary>
+        /// <param name="inputX">平行方向への入力情報</param>
+        /// <param name="inputZ">水平方向への入力情報</param>
+        /// <returns>移動方向</returns>
+		Vector3 CameraRelativeInput(float inputX, float inputZ){
+			//x,z方向にのみに沿ったカメラに対するベクトル  
+			Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
+			forward.y = 0;
+            //正規化
+			forward = forward.normalized;
+			//カメラに対する右ベクトル
+			Vector3 right = new Vector3(forward.z, 0, -forward.x);
+			Vector3 relativeVelocity = inputHorizontal * right + inputVertical * forward;
+			//全ての方向への正規化を行う
+			if(relativeVelocity.magnitude > 1){
+				relativeVelocity.Normalize();
+			}
+			return relativeVelocity;
+		}
+
+        /// <summary>
+        /// 何らかのボタンを入力しているか
+        /// </summary>
+		public bool HasAnyInput(){
+			if(allowedInput && moveInput != Vector3.zero && aimInput != Vector2.zero && inputJump != false){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		
+        /// <summary>
+        /// 移動関連のボタンを入力しているか
+        /// </summary>
+		public bool HasMoveInput(){
+			if(allowedInput && moveInput != Vector3.zero){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		
+        /// <summary>
+        /// 
+        /// </summary>
+		public bool HasAimInput(){
+			if(allowedInput && (aimInput.x < -0.8f || aimInput.x > 0.8f) || (aimInput.y < -0.8f || aimInput.y > 0.8f)){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+	}
+}
